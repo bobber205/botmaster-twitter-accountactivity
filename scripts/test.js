28452d5ed69aebb45ed1f9feac1b075e6d6daf4d
Alex Wait <alex@chirpify.com>
2018-03-28 13:44:07 -0700

    Define not late too

---
+++ scripts/test.js
@@ -0,0 +1,24 @@
+const _ = require('lodash');
+
+var user_one = [1,2,3,4,5];
+
+var user_two =  [1,2,4,10];
+
+// This is your array of arrays of user profiles split by " "
+var array_of_arrays = [user_one, user_two];
+
+var uniques = _.chain(array_of_arrays).flattenDeep().uniq().compact().value();
+
+var all_flattened = _.flattenDeep(array_of_arrays)
+
+console.log(uniques)
+
+console.log(all_flattened)
+
+var final_result = _.map(uniques, (current) => {
+    return {value: current, count: _.filter(all_flattened, (current_other) => {return current == current_other}).length}
+});
+
+final_result = _.chain(final_result).sortBy('count').reverse().value();
+
+console.log(final_result)
