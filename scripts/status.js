ac190bda19d1b391c1bc5a81e787ea69d20a7554
Alex Wait <alex@chirpify.com>
2018-04-13 11:24:31 -0700

    Stop sending images for now

---
+++ scripts/status.js
@@ -0,0 +1,47 @@
+var Twitter = require("twitter");
+
+var config = {
+  consumer_key: process.env.CHRP_TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.CHRP_TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.CHRP_TWITTER_ACCESS,
+  access_token_secret: process.env.CHRP_TWITTER_SECRET
+  // bearer_token: process.env.BEARER_TOKEN
+};
+
+var client = new Twitter(config);
+
+
+    function checkStatus(mediaId) {
+      console.log("checkStatus mediaID is", mediaId);
+      return makeGet("media/upload", {
+        command: "STATUS",
+        media_id: mediaId
+      })
+        .then(data => {
+          console.log("status data", data);
+          return mediaId;
+        })
+        .catch((error) => {
+          console.log('error???', error)
+          console.log(arguments);
+        });
+    }
+
+
+  function makeGet(endpoint, params) {
+    return new Promise((resolve, reject) => {
+      client.get(endpoint, params, (error, data, response) => {
+        if (error) {
+          console.log("ERROR ON POST", error);
+          reject(error);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  }
+
+  global.media_id = "984837731976015872";
+
+  console.log(`checking status of ${global.media_id}`);
+  checkStatus(global.media_id);
