25f2830f979387f12facaab96d2b11999b8371b9
Alex Wait <alex@chirpify.com>
2018-04-17 10:44:35 -0700

    Adding extra lives to setup

--- scripts/setup_configuration.js
+++ scripts/setup_configuration.js
@@ -1,5 +1,6 @@
 const redisHelpers = require("../lib/helpers/redisHelpers");
 const redis = require("redis");
+const _ = require('lodash');
 
 const redisClient = redis.createClient(process.env.REDIS_URL);
 
@@ -8,8 +9,16 @@ var config_object = {
   right_media_assets: ["985993017864601600"]
 };
 
+var quiz_handle = process.env.QUIZ_HANDLE || "IQtrivia";
+
+var handles_with_extra_lives = ["bobber205", "ChrisTeso"];
+
 redisClient.on("connect", function(err) {
   redisClient.set(redisHelpers.getConfigurationKey(), JSON.stringify(config_object), (result) => {
       console.log("Configuration Set", config_object, result);
   });
+  _.each(handles_with_extra_lives, (handle) => {
+    redisClient.hmset(redisHelpers.getExtraLifeHashKey(quiz_handle), handle, "1", redis.print);
+  });
 });
+
