f96f4a9d4e080f155965b2b535bccebbd1180b4e
Alex Wait <alex@chirpify.com>
2018-04-19 16:23:39 -0700

    Sorry little guy

--- scripts/upload_images.js
+++ scripts/upload_images.js
@@ -12,6 +12,7 @@ bluebird.promisifyAll(redis.Multi.prototype);
 
 const _ = require("lodash");
 
+// join(imageUploader.uploadImages('test')).then((all_ids) => {
 join(imageUploader.uploadImages('wrong'), imageUploader.uploadImages('right')).then((all_ids) => {
     var wrong_ids = all_ids[0];
     var right_ids = all_ids[1];
@@ -24,5 +25,20 @@ join(imageUploader.uploadImages('wrong'), imageUploader.uploadImages('right')).t
         config.wrong_media_assets = wrong_ids;
         redisClient.set(redisHelpers.getConfigurationKey(), JSON.stringify(config), redis.print);
     });
-
 });
+// var payload  = {
+//   event: {
+//     type: "message_create",
+//     message_create: {
+//       target: {
+//         recipient_id: "15588742"
+//       },
+//       message_data: {
+//         text: "Tap Your Answer Below"
+//       }
+//     }
+//   }
+// };
+
+
+// imageUploader.sendDM(payload);
