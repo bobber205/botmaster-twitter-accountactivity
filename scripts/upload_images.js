221287b8cfd793dbb84c2c06a1588ca87938f8e1
Alex Wait <alex@chirpify.com>
2018-04-20 13:44:49 -0700

    wip

--- scripts/upload_images.js
+++ scripts/upload_images.js
@@ -13,16 +13,19 @@ bluebird.promisifyAll(redis.Multi.prototype);
 const _ = require("lodash");
 
 // join(imageUploader.uploadImages('test')).then((all_ids) => {
-join(imageUploader.uploadImages('wrong'), imageUploader.uploadImages('right')).then((all_ids) => {
+join(imageUploader.uploadImages('wrong'), imageUploader.uploadImages('right'), imageUploader.uploadImages('wait')).then((all_ids) => {
     var wrong_ids = all_ids[0];
     var right_ids = all_ids[1];
+    var wait_ids = all_ids[2];
     console.log("Wrong ids", wrong_ids);
     console.log("Right Ids", right_ids);
+    console.log("Wait Ids", wait_ids);
     redisClient.getAsync(redisHelpers.getConfigurationKey()).then((config) => {
         if (!config) config = "{}";
         config = JSON.parse(config);
         config.right_media_assets = right_ids;
         config.wrong_media_assets = wrong_ids;
+        config.wait_media_assets = wait_ids;
         redisClient.set(redisHelpers.getConfigurationKey(), JSON.stringify(config), redis.print);
     });
 });
