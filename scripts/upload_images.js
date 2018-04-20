60d7527ecf01be0d26debe074c29859bcc3788dd
Alex Wait <alex@chirpify.com>
2018-04-20 15:09:30 -0700

    Adding a late image

--- scripts/upload_images.js
+++ scripts/upload_images.js
@@ -13,10 +13,11 @@ bluebird.promisifyAll(redis.Multi.prototype);
 const _ = require("lodash");
 
 // join(imageUploader.uploadImages('test')).then((all_ids) => {
-join(imageUploader.uploadImages('wrong'), imageUploader.uploadImages('right'), imageUploader.uploadImages('wait')).then((all_ids) => {
+join(imageUploader.uploadImages('wrong'), imageUploader.uploadImages('right'), imageUploader.uploadImages('wait'), imageUploader.uploadImages('late')).then((all_ids) => {
     var wrong_ids = all_ids[0];
     var right_ids = all_ids[1];
     var wait_ids = all_ids[2];
+    var late_ids = all_ids[3];
     console.log("Wrong ids", wrong_ids);
     console.log("Right Ids", right_ids);
     console.log("Wait Ids", wait_ids);
@@ -26,6 +27,7 @@ join(imageUploader.uploadImages('wrong'), imageUploader.uploadImages('right'), i
         config.right_media_assets = right_ids;
         config.wrong_media_assets = wrong_ids;
         config.wait_media_assets = wait_ids;
+        config.late_media_assets = late_ids;
         redisClient.set(redisHelpers.getConfigurationKey(), JSON.stringify(config), redis.print);
     });
 });
