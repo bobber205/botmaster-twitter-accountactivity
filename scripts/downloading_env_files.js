a5dbb44038c316bac3d47bdbffc31d03a0a0e66f
Alex Wait <alex@chirpify.com>
2018-05-07 13:10:53 -0700

    update

---
+++ scripts/downloading_env_files.js
@@ -0,0 +1,51 @@
+const s3 = require('s3');
+const Promise = require("bluebird");
+
+var client = s3.createClient({
+    maxAsyncS3: 20, // this is the default
+    s3RetryCount: 3, // this is the default
+    s3RetryDelay: 1000, // this is the default
+    multipartUploadThreshold: 20971520, // this is the default (20 MB)
+    multipartUploadSize: 15728640, // this is the default (15 MB)
+    s3Options: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_S3_REGION,
+        // endpoint: 's3.yourdomain.com',
+        // sslEnabled: false
+        // any other options are passed to new AWS.S3()
+        // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
+    },
+});
+
+var params = {
+    localFile: "config/accounts.json",
+
+    s3Params: {
+        Bucket: "iqtrivia",
+        Key: "environment/accounts.json",
+    },
+}
+
+var my_promise = new Promise((resolve, reject) => {
+        var uploader = client.downloadFile(params);
+        uploader.on('error', function (err) {
+            console.error("unable to upload:", err.stack);
+        });
+        uploader.on('progress', function () {
+            console.log("progress", uploader.progressMd5Amount,
+                uploader.progressAmount, uploader.progressTotal);
+        });
+        uploader.on('end', function () {
+            resolve();
+        });
+    });
+
+async function uploadFile() {
+    console.log("Waiting...");
+    await my_promise;
+    console.log("Done Uploading!")
+}
+
+uploadFile();
+
