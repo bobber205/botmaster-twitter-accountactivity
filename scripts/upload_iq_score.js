deb20255d325345107c47e441a6f36946e202122
Alex Wait <alex@chirpify.com>
2018-05-14 10:31:09 -0700

    Fix iq uploading

--- scripts/upload_iq_score.js
+++ scripts/upload_iq_score.js
@@ -14,22 +14,21 @@ const _ = require("lodash");
 
 var bot_handle = process.env.BOT_HANDLE;
 
-var defs = _.map(['iq_score'], (dir_name) => {return imageUploader.uploadImages(dir_name)});
+imageUploader.onEnvReady().then(() => {
+    var defs = _.map(['iq_score'], (dir_name) => {return imageUploader.uploadImages(dir_name)});
+    join(...defs).then((all_ids) => {
+        all_ids = Object.assign.apply(Object, _.flatten(all_ids));
+        var iq_media_assets = all_ids.iq_score;
 
-console.log("Deferreds are", defs);
-
-join(...defs).then((all_ids) => {
-    all_ids = Object.assign.apply(Object, _.flatten(all_ids));
-    var iq_media_assets = all_ids.iq_score;
-
-    console.log("IQ Ids", iq_media_assets);
-    if (iq_media_assets)
-        console.log("IQ Media Asset Length", iq_media_assets.length);
-    redisClient.getAsync(redisHelpers.getConfigurationKeyForHandle(bot_handle)).then((config) => {
-        if (!config) config = "{}";
-        config = JSON.parse(config);
-        if (iq_media_assets && iq_media_assets.length)
-            config.iq_media_assets = iq_media_assets;
-        redisClient.set(redisHelpers.getConfigurationKeyForHandle(bot_handle), JSON.stringify(config), redis.print);
+        console.log("IQ Ids", iq_media_assets);
+        if (iq_media_assets)
+            console.log("IQ Media Asset Length", iq_media_assets.length);
+        redisClient.getAsync(redisHelpers.getConfigurationKeyForHandle(bot_handle)).then((config) => {
+            if (!config) config = "{}";
+            config = JSON.parse(config);
+            if (iq_media_assets && iq_media_assets.length)
+                config.iq_media_assets = iq_media_assets;
+            redisClient.set(redisHelpers.getConfigurationKeyForHandle(bot_handle), JSON.stringify(config), redis.print);
+        });
     });
 });
