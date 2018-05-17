b55c0e3f71bd9afff33c1b1185a25d623472e866
Alex Wait <alex@chirpify.com>
2018-05-17 09:42:42 -0700

    update

---
+++ scripts/set_extra_lives.js
@@ -0,0 +1,20 @@
+const redisHelpers = require("../lib/helpers/redisHelpers");
+const redis = require("redis");
+const _ = require('lodash');
+
+const redisClient = redis.createClient(process.env.REDIS_URL);
+
+var quiz_handle = process.env.BOT_HANDLE;
+
+var handles_with_extra_lives = ["owlsfan954", "chrisbarnett01", "AkhilaMidde", "bobber205", "christeso", "jingwait"];
+  
+redisClient.on("connect", function(err) {
+  console.log(`Handles with Extra Lives Are!! ${handles_with_extra_lives}`)
+  _.each(handles_with_extra_lives, (handle) => {
+    console.log(`Setting ${handle} to 1`);
+    redisClient.hmset(redisHelpers.getExtraLifeHashKey(quiz_handle), handle, "1", redis.print);
+  });
+});
+
+
+
