0bbbea90d87dd1ab5a1d03e4f020b1dd6c4c6751
Alex Wait <alex@chirpify.com>
2018-05-22 14:18:53 -0700

    update

---
+++ scripts/list_extra_lives.js
@@ -0,0 +1,21 @@
+const redisHelpers = require("../lib/helpers/redisHelpers");
+const redis = require("redis");
+const _ = require('lodash');
+
+const bluebird = require('bluebird');
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+const redisClient = redis.createClient(process.env.REDIS_URL);
+// const redisClient = redis.createClient();
+
+var quiz_handle = process.env.BOT_HANDLE;
+
+console.log(`Handle is ${quiz_handle}`);
+
+redisClient.on("connect", (error) => {
+    redisClient.hgetallAsync(redisHelpers.getExtraLifeHashKey(quiz_handle.toLowerCase())).then(result => {
+        console.log("Extra Lives Are:")
+        console.log(result)
+    });
+});
