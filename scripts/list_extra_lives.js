94eccf1ad556c8da6787c7f8a3228ab251fb971f
Alex Wait <alex@chirpify.com>
2018-05-24 11:03:05 -0700

    Update

--- scripts/list_extra_lives.js
+++ scripts/list_extra_lives.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const redisHelpers = require("../lib/helpers/redisHelpers");
 const redis = require("redis");
 const _ = require('lodash');
@@ -11,9 +13,22 @@ const redisClient = redis.createClient(process.env.REDIS_URL);
 
 var quiz_handle = process.env.BOT_HANDLE;
 
+var option = _.drop(process.argv, 2);
+
+var do_full = option[0] == 'full' ? true: false
+
+
 redisClient.on("connect", (error) => {
     redisClient.hgetallAsync(redisHelpers.getExtraLifeHashKey(quiz_handle.toLowerCase())).then(result => {
         console.log("Extra Lives Are:")
-        console.log(result)
+        result = _.map(result, (current, index) => {
+            if (do_full) return { [current]: index};
+            if (current == '1') return {
+                [current]: index
+            };
+            return null;
+        });
+        console.log(result);
+        console.log(`There are ${result.length} total extra lives set`);
     });
 });
