4e1d417ed8b5d662b454a70272de08701d0e3c23
Alex Wait <alex@chirpify.com>
2018-06-12 10:39:12 -0700

    yay update

--- scripts/test_sms.js
+++ scripts/test_sms.js
@@ -17,33 +17,33 @@ const sender_phone_sid = `PN1150a94f14e7074bb3aa2b8fba4728ad`;
 
 
 
-// client.messaging.services(sid)
-//     .phoneNumbers
-//     .create({
-//         phoneNumberSid: 'PN1150a94f14e7074bb3aa2b8fba4728ad'
-//     })
-//     .then(phone_number => console.log(phone_number.sid))
-//     .done();
-
-
 client.messaging.services(sid)
     .phoneNumbers
-    .each(phoneNumbers => console.log(phoneNumbers.sid));
+    .create({
+        phoneNumberSid: 'PN09404858448eed8b094b021f150abe56'
+    })
+    .then(phone_number => console.log(phone_number.sid))
+    .done();
+
+
+// client.messaging.services(sid)
+//     .phoneNumbers
+//     .each(phoneNumbers => console.log(phoneNumbers.sid));
 
 
 
-// const numbers = ['1 541-591-9824', '+1 541-636-7552'];
-const numbers = ['1 541-591-9824'];
-// const numbers = ['+15415919824'];
-// const numbers = ['+15033296310', '+15415919824'];
+// // const numbers = ['1 541-591-9824', '+1 541-636-7552'];
+// const numbers = ['1 541-591-9824'];
+// // const numbers = ['+15415919824'];
+// // const numbers = ['+15033296310', '+15415919824'];
 
-numbers.forEach((number) => {
-        client.messages
-            .create({
-                body: `Hi it’s IQ! We’ll let you know when the game starts. To unsubscribe from reminders just reply with "stop".`,
-                messagingServiceSid: sid,
-                to: number
-            })
-            .then(message => console.log(message.sid))
-            .done();
-})
+// numbers.forEach((number) => {
+//         client.messages
+//             .create({
+//                 body: `Hi it’s IQ! We’ll let you know when the game starts. To unsubscribe from reminders just reply with "stop".`,
+//                 messagingServiceSid: sid,
+//                 to: number
+//             })
+//             .then(message => console.log(message.sid))
+//             .done();
+// })
