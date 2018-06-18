d383f9382d2a3d113506324140a06ce8ccb5947e
Alex Wait <alex@chirpify.com>
2018-06-18 14:27:26 -0700

    yay update

--- scripts/send_out_sms_reminders.js
+++ scripts/send_out_sms_reminders.js
@@ -24,8 +24,7 @@ mysql_helper.getDBConnection().then(function (conn) {
     conn.query(sql_statement).then((res) => {
         var numbers = map(res, 'phone_number');
         console.log(`Read in ${numbers.length} phone numbers to send out a reminder to.`);
-        var numbers = ['+15033296310', '+15415919824'];
-
+        // var numbers = ['+15033296310', '+15415919824'];
         numbers.forEach((number) => {
             client.messages
                 .create({
