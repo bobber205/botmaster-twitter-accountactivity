470cf3e300081fcdb9ed6f770ae501d23a44bc3a
Alex Wait <alex@chirpify.com>
2018-07-05 14:53:00 -0700

    yay update

--- scripts/list_extra_lives.js
+++ scripts/list_extra_lives.js
@@ -29,7 +29,7 @@ redisClient.on("connect", (error) => {
             };
             return null;
         }).compact().value();
-        console.log(JSON.stringify(myObject, null, 4));
+        console.log(JSON.stringify(result, null, 4));
         console.log(`There are ${result.length} total extra lives set`);
     });
 });
